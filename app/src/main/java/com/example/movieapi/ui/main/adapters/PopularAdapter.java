f72ab43c77f569e8ea8fcc1b64058f3cf7855063
mahmo <mahmoud.mostafa20112011@gmail.com>
2022-12-13 01:04:04 +0200

    get popular movies poster image

--- app/src/main/java/com/example/movieapi/ui/main/adapters/PopularAdapter.java
+++ app/src/main/java/com/example/movieapi/ui/main/adapters/PopularAdapter.java
@@ -14,34 +14,42 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.movieapi.R;
 import com.example.movieapi.pojo.MovieModel;
+import com.example.movieapi.utils.Credentials;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.PopularViewHolder> {
     private List<MovieModel> mList;
-    public PopularAdapter(List<MovieModel> movieList){
-        mList=movieList;
+
+    public PopularAdapter(List<MovieModel> movieList) {
+        mList = movieList;
     }
 
     @NonNull
     @Override
     public PopularViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new PopularViewHolder(LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.popular_list_item,parent, false));
+                .inflate(R.layout.popular_list_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull PopularViewHolder holder, int position) {
-        MovieModel currentMovie=mList.get(position);
-        Glide.with(holder.itemView.getContext())
-                .load(currentMovie.getPoster_path())
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop()
-                .placeholder(R.drawable.ic_movie_place_holder)
-                .into(holder.movieImg);
-        holder.movieName.setText(currentMovie.getTitle());
-        holder.movieRate.setRating((currentMovie.getVote_average()/2));
+        MovieModel currentMovie = mList.get(position);
+        if (currentMovie.getPoster_path() != null) {
+            Glide.with(holder.itemView.getContext())
+                    .load(Credentials.IMAGE_URL + currentMovie.getPoster_path())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_movie_place_holder)
+                    .into(holder.movieImg);
+
+        }
+        if (currentMovie.getTitle()!=null){
+            holder.movieName.setText(currentMovie.getTitle());
+        }else{
+            holder.movieName.setText("null");
+        }
+        holder.movieRate.setRating((currentMovie.getVote_average() / 2));
     }
 
     @Override
@@ -49,16 +57,16 @@ public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.PopularV
         return mList.size();
     }
 
-    public static class PopularViewHolder extends RecyclerView.ViewHolder{
+    public static class PopularViewHolder extends RecyclerView.ViewHolder {
         ImageView movieImg;
         TextView movieName;
         RatingBar movieRate;
 
         public PopularViewHolder(@NonNull View itemView) {
             super(itemView);
-            movieImg=itemView.findViewById(R.id.movie_img);
-            movieName=itemView.findViewById(R.id.movie_title_txt);
-            movieRate=itemView.findViewById(R.id.movie_rating_bar);
+            movieImg = itemView.findViewById(R.id.movie_img);
+            movieName = itemView.findViewById(R.id.movie_title_txt);
+            movieRate = itemView.findViewById(R.id.movie_rating_bar);
         }
     }
 }
--- app/src/main/java/com/example/movieapi/utils/Credentials.java
+++ app/src/main/java/com/example/movieapi/utils/Credentials.java
@@ -3,5 +3,6 @@ package com.example.movieapi.utils;
 public class Credentials {
     public static final String BASE_URL="https://api.themoviedb.org/3/";
     public static final String API_KEY="b13e8ea5ebb980fe5fd6a5a83e2b478c";
+    public static final String IMAGE_URL="https://image.tmdb.org/t/p/w500";
 
 }

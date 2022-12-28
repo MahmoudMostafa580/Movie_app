4076db942bf3c1a9ba59f3cf75c54719ef1610c6
mahmo <mahmoud.mostafa20112011@gmail.com>
2022-12-28 18:25:01 +0200

    set logo and splash screen

--- app/src/main/java/com/example/movieapi/ui/launcher/LauncherActivity.java
+++ app/src/main/java/com/example/movieapi/ui/launcher/LauncherActivity.java
@@ -2,10 +2,13 @@ package com.example.movieapi.ui.launcher;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -20,10 +23,18 @@ public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        splashScreen.setKeepOnScreenCondition(() -> true);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         mSharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE);
         boolean isLogged = mSharedPreferences.getBoolean("is_logged", false);
